3222ec95855479cf9c7ecdae78be6e104938b5ab
hejia <hejia@egova.com.cn>
2023-03-14 11:10:39 +0800

    [M]feature-20230314-test

---
+++ demo1/src/main/java/com/hejia/demo/VersionDemo.java
@@ -0,0 +1,10 @@
+package com.hejia.demo;
+
+import org.junit.Test;
+
+public class VersionDemo {
+    @Test
+    public void test1() {
+        System.out.println("feature-20230314-test");
+    }
+}
